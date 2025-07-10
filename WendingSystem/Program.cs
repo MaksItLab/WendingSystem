fceaea19c93bdb5e3b86e7a64ac729506b86b716
Anno-45 <nikita20061108@mail.ru>
2025-07-10 17:01:54 +0300

    добавил переменную "Ballanse" начал реализацию методов "InsertMoney", "BuyProduct", "ReturnChange"

--- WendingSystem/Program.cs
+++ WendingSystem/Program.cs
@@ -27,11 +27,26 @@ namespace WendingSystem
             }
            
         }
-        class VendingMachine : Product
+        class VendingMachine
         {
-            public VendingMachine(string name, decimal price, int quantity) : base(name, price, quantity)
+            public decimal Ballanse { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+
+            
+            public VendingMachine(string name, decimal price, int quantity, decimal ballanse )
             {
+                Name = name;
+                Price = price;
+                Quantity = quantity;
+                Ballanse = ballanse;
             }
+            public void DisplayProducts(string name, decimal price, int quantity)
+            {
+                
+            }
+        
 
             public void BuyProduct(string name, decimal price, int quantity)
             {
@@ -40,6 +55,14 @@ namespace WendingSystem
 
                 Console.WriteLine("выберите кол-во: ");
                 int quantity1 = int.Parse(Console.ReadLine());
+            }
+            public void InsertMoney()
+            {
+
+            }
+            public void ReturnChange()
+            {
+
             }
         }
     }
