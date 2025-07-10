263013f96b0bbcb6f14fc89dd0365fbfd28a7324
MaksItLab <maksim.ardintsev@mail.ru>
2025-07-10 13:34:14 +0300

    Добавьте файлы проекта.

---
+++ WendingSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WendingSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        class Product
+        {
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+
+            public Product(string name, decimal price, int quantity)
+            {
+                Name = name;
+                Price = price;
+                Quantity = quantity;
+            }
+        }
+    }
+}
