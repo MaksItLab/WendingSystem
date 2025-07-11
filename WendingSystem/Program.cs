408a20c12c59abe38cf8181ff51a654dcdc4059a
Anno-45 <nikita20061108@mail.ru>
2025-07-11 13:42:05 +0300

    реализую метод "DisplayProducts"

--- WendingSystem/Program.cs
+++ WendingSystem/Program.cs
@@ -36,9 +36,110 @@ namespace WendingSystem
                 Ballance = ballance;
             }
 
-            public void DisplayProducts(string name, decimal price, int quantity)
+            public void DisplayProducts()
             {
-                
+                var rand = new Random();
+                var randcena = new Random();
+                Console.WriteLine("список ассортимента продуктов выглядит: ");
+                List<string> spisok = new List<string>(10) {"(яблоко) индекс товара: №12774 ", "(ананас) индекс товара: №65345 ", "(апельсин) индекс товара: №68442 ", "(авокадо) индекс товара: №31754 ", "(банан) индекс товара: №18732 ", "(виноград) индекс товара: №98431 ", "(персик) индекс товара: №87575 ", "(груша) индекс товара: №71490 ", "(картошка) индекс товара: №00642 ", "(лимон) индекс товара: №50492 ", };
+                int elementnumber = 0;
+               
+                for (int i = 0; i < spisok.Count; i++)
+                {
+                    elementnumber++;
+                    Console.WriteLine(elementnumber + " " + spisok[i] + " цена: " + randcena.Next(75, 250) + "руб/100гр. " + " в наличии имеется: " + rand.Next(10, 1010));
+                }
+                int element = int.Parse(Console.ReadLine());
+                if (element == 12774)
+                {
+                    Console.WriteLine("вы хотите купить именно этот товар (яблоко)" + "\n yes/no ");
+                    string element1 = Console.ReadLine();
+                    if (element1 == "yes")
+                    {
+                        Console.WriteLine("товар успешно добавлен в корзину!");
+                    }
+                }
+                if (element == 65345)
+                {
+                    Console.WriteLine("вы хотите купить именно этот товар (ананас)" + "\n yes/no ");
+                    string element1 = Console.ReadLine();
+                    if (element1 == "yes")
+                    {
+                        Console.WriteLine("товар успешно добавлен в корзину!");
+                    }
+                }
+                if (element == 68442)
+                {
+                    Console.WriteLine("вы хотите купить именно этот товар (апельсин)" + "\n yes/no ");
+                    string element1 = Console.ReadLine();
+                    if (element1 == "yes")
+                    {
+                        Console.WriteLine("товар успешно добавлен в корзину!");
+                    }
+                }
+                if (element == 31754)
+                {
+                    Console.WriteLine("вы хотите купить именно этот товар (авокадо)" + "\n yes/no ");
+                    string element1 = Console.ReadLine();
+                    if (element1 == "yes")
+                    {
+                        Console.WriteLine("товар успешно добавлен в корзину!");
+                    }
+                }
+                if (element == 18732)
+                {
+                    Console.WriteLine("вы хотите купить именно этот товар (банан)" + "\n yes/no ");
+                    string element1 = Console.ReadLine();
+                    if (element1 == "yes")
+                    {
+                        Console.WriteLine("товар успешно добавлен в корзину!");
+                    }
+                }
+                if (element == 98431)
+                {
+                    Console.WriteLine("вы хотите купить именно этот товар (виноград)" + "\n yes/no ");
+                    string element1 = Console.ReadLine();
+                    if (element1 == "yes")
+                    {
+                        Console.WriteLine("товар успешно добавлен в корзину!");
+                    }
+                }
+                if (element == 87575)
+                {
+                    Console.WriteLine("вы хотите купить именно этот товар (персик)" + "\n yes/no ");
+                    string element1 = Console.ReadLine();
+                    if (element1 == "yes")
+                    {
+                        Console.WriteLine("товар успешно добавлен в корзину!");
+                    }
+                }
+                if (element == 71490)
+                {
+                    Console.WriteLine("вы хотите купить именно этот товар (груша)" + "\n yes/no ");
+                    string element1 = Console.ReadLine();
+                    if (element1 == "yes")
+                    {
+                        Console.WriteLine("товар успешно добавлен в корзину!");
+                    }
+                }
+                if (element == 00642)
+                {
+                    Console.WriteLine("вы хотите купить именно этот товар (картошка)" + "\n yes/no ");
+                    string element1 = Console.ReadLine();
+                    if (element1 == "yes")
+                    {
+                        Console.WriteLine("товар успешно добавлен в корзину!");
+                    }
+                }
+                if (element == 50492)
+                {
+                    Console.WriteLine("вы хотите купить именно этот товар (лимон)" + "\n yes/no ");
+                    string element1 = Console.ReadLine();
+                    if (element1 == "yes")
+                    {
+                        Console.WriteLine("товар успешно добавлен в корзину!");
+                    }
+                }
             }
         
 
@@ -48,9 +149,10 @@ namespace WendingSystem
                 string opiration = Console.ReadLine();
 
                 Console.WriteLine("к оплате: " + " " + "руб" + "\nваш баланс составляет: " + " " + "руб" + "\nсовершить операцию? yes/no");
+                string opiration1 = Console.ReadLine();
 
                 Console.WriteLine("покупка совершена успешно! " + "\nхотите совершить следующую покупку? " + "\n yes/no ");
-                string opiration1 = Console.ReadLine();
+                string opiration2 = Console.ReadLine();
             }
             public void InsertMoney()
             {
